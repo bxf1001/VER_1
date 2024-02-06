87ab87fa5275d774ca24d03b80b049b18818dbce
bxf1001 <149566631+bxf1001@users.noreply.github.com>
2024-02-06 11:06:52 +0530

    Update Pywintest.py

--- Pywintest.py
+++ Pywintest.py
@@ -1,4 +1,12 @@
-from pywinauto.application import Application
-app = Application().start("C:\\Program Files\\WindowsApps\\5319275A.WhatsAppDesktop_2.2401.5.0_x64__cv1g1gvanyjgm\\WhatsApp.exe")  # Replace with the actual path
+import subprocess
+import time
+from pywinauto import Desktop, Application
+number = 8056099417
+app = Application(backend='uia').start(r"cmd.exe /c start shell:appsFolder\5319275A.WhatsAppDesktop_cv1g1gvanyjgm!App", create_new_console=True, wait_for_idle=False)
+app = Application(backend='uia').connect(title_re=".*WhatsApp.*") # Replace with the actual path
+url = f"whatsapp://send?phone=+91{number}"
+subprocess.Popen(["cmd", "/C", f"start {url}"], shell=True)
+time.sleep(0.25)
+subprocess.Popen(["cmd", "/C", f"start {url}"], shell=True)
 app.WhatsAppDialog.VideoCallButton.click()  # Replace with actual control names
-app.WhatsAppDialog.VideoCallButton.click_input()  # Simulates cursor movement and clicks
+#app.WhatsAppDialog.VideoCallButton.click_input()  # Simulates cursor movement and clicks
